873db69e8c1c070be42fa02d8bc54d6a991df34d
chayan007 <sonicxxx7@gmail.com>
2020-03-31 15:33:58 +0530

    College related models created

--- college/models.py
+++ college/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from exam.models import Exam
+
+
+class College(models.Model):
+    """Model for college."""
+
+    class OwnershipChoices(models.IntegerChoices):
+        """Choices for ownership."""
+        PRIVATE = 0, _('Private')
+        PUBLIC = 1, _('Public')
+        OTHER = 2, _('Other')
+
+    class InstitutionType(models.IntegerChoices):
+        """Choices for Institution type."""
+        GRADUATION_COLLEGE = 0,
+        DIPLOMA_COLLEGE = 1,
+        VOCATIONAL_TRAINING = 2
+
+    full_name = models.CharField(max_length=255)
+    abbreviated_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    brochure = models.FileField(upload_to='college/brochure/')
+    ownership = models.IntegerField(choices=OwnershipChoices.choices)
+    college_type = models.IntegerField(choices=InstitutionType.choices)
+    date_of_establishment = models.DateField()
+    about = models.TextField()
+
+
+class EntranceExam(models.Model):
+    """Specify which entrance exams for which college."""
+
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+
+
+class Degree(models.Model):
+    """Model of degrees."""
+
+    full_name = models.CharField(max_length=255)
+    abbreviated_name = models.CharField(max_length=50)
+
+
+class Stream(models.Model):
+    """Model for streams."""
+
+    class DegreeType(models.Choices):
+        """Types of degree."""
+        UNDER_GRADUATE = 'UG'
+        POST_GRADUATE = 'PG'
+        DIPLOMA = 'D'
+        DOCTORATE = 'PHD'
+        OTHERS = 'OT'
+
+    name = models.CharField(max_length=60)
+    degree_type = models.CharField(max_length=3, choices=DegreeType.choices)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE, null=True)
+
+
+class OfficialContact(models.Model):
+    """Model to store official college contacts."""
+
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.CharField(max_length=40, null=True)
+    full_name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=50)
