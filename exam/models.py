d9ebf775cc1801e45ffbca55f3eb7ea36accd3e0
chayan007 <sonicxxx7@gmail.com>
2020-04-10 11:40:06 +0530

    Exam models

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    """Model for exams."""
+
+    class ExamLevel(models.Choices):
+        """Types of exam level."""
+        NATIONAL = 'NT'
+        INTERNATIONAL = 'INT'
+        STATE = 'ST'
+        DISTRICT = 'DT'
+
+    class BasicEligibilityChoices(models.IntegerChoices):
+        """Eligibility choices."""
+        TEN = 1, _('Matric Pass')
+        TWELVE = 2, _('HS Pass')
+        UNDER_GRADUATE = 3, _('Under Graduate')
+        POST_GRADUATE = 4, _('Post Graduate')
+        OTHER = 0, _('Other')
+
+    full_name = models.CharField(max_length=200)
+    abbreviated_name = models.CharField(max_length=200)
+    conducting_body_full_name = models.CharField(max_length=200)
+    conducting_body_abbreviated_name = models.CharField(max_length=200)
+    official_website = models.CharField(max_length=200)
+    tentative_date = models.DateField()
+    exam_level = models.CharField(max_length=5, choices=ExamLevel.choices)
+    eligibility = models.IntegerField(choices=BasicEligibilityChoices.choices)
+    document = models.FileField(upload_to='exams/documents/')
+    about = models.TextField()
+    cutoff_details = models.TextField()
+
+    def __str__(self):
+        return self.full_name
