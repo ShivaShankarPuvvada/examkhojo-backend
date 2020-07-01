7f365bad1f65de1dde82b73008e0dd9e57288669
chayan007 <sonicxxx7@gmail.com>
2020-07-01 16:18:10 +0530

    gallery link

--- college/admin.py
+++ college/admin.py
@@ -6,6 +6,7 @@ from college.models import *
 admin.site.site_header = 'ExamKhojo'
 
 admin.site.register(College)
+admin.site.register(Gallery)
 admin.site.register(EntranceExam)
 admin.site.register(Degree)
 admin.site.register(Stream)
