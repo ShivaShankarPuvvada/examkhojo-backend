b327bea6b8b083a03c43b476755a2f5d835b2289
chayan007 <sonicxxx7@gmail.com>
2020-07-03 11:10:36 +0530

    exam models added

--- exam/admin.py
+++ exam/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from exam.models import Exam
+from exam.models import *
 
 admin.site.site_header = 'ExamKhojo'
 
+admin.site.register(Exam)
+admin.site.register(ExamDate)
+admin.site.register(ExamCutoff)
 admin.site.register(Exam)
