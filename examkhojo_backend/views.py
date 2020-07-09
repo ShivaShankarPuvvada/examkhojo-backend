d11320d245c4120d570823b30e9199d1e6f61bf2
chayan007 <sonicxxx7@gmail.com>
2020-07-09 16:02:05 +0530

    KR - comment added

--- examkhojo_backend/views.py
+++ examkhojo_backend/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 def get_home(request):
     """Get index page."""
+    # This is static page (dist/index.html) - working fine (useless)
+    # This is a jinja layout (pages/index.html) - not working fine (main part)
     return render(request, 'dist/index.html')
