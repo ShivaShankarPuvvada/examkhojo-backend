1bc1876070adef854f81e0dc511e0887d7682ae4
chayan007 <sonicxxx7@gmail.com>
2020-07-09 16:04:18 +0530

    KR - comment added

--- examkhojo_backend/views.py
+++ examkhojo_backend/views.py
@@ -5,4 +5,5 @@ def get_home(request):
     """Get index page."""
     # This is static page (dist/index.html) - working fine (useless)
     # This is a jinja layout (pages/index.html) - not working fine (main part)
+    # Also check I can serve pages when DEBUG=True (in dev stage)
     return render(request, 'dist/index.html')
